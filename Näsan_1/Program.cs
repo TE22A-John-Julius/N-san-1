b19b2c6491a485bd236138e4ba9d0b57c94f0bd8
Julius John <julius.john@elev.ga.ntig.se>
2024-11-12 10:40:07 +0100

    Basic kod, lösenords while loop klar

--- Näsan_1/Program.cs
+++ Näsan_1/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// if (6 >= 3)
+// {
+//     Console.WriteLine("Hello World!");
+// }
+
+/*-----------------------------------------------------*/
+
+// Console.WriteLine("Give your username:");
+
+// string user = Console.ReadLine().ToLower();
+
+// if (user == "noname")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+
+/*-----------------------------------------------------*/
+
+// Console.WriteLine("Give your username:");
+
+// string user = Console.ReadLine().ToLower();
+
+// Console.WriteLine($"{user} give your password:");
+
+// string password = Console.ReadLine().ToLower();
+
+// if (user == "noname" && password == "nopass")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+
+/*-----------------------------------------------------*/
+
+// Console.WriteLine("Give your username:");
+
+// string user = Console.ReadLine().ToLower();
+
+// Console.WriteLine($"{user} give your password:");
+
+// string password = Console.ReadLine().ToLower();
+
+// if (user == "noname" && password == "nopass")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// else
+// {
+//     Console.WriteLine("Wrong username or password!");
+
+// }
+/*-----------------------------------------------------*/
+
+// int amt = 1;
+
+// while (amt <= 32)
+// {
+//     Console.WriteLine($"Hello World! {amt}");
+//     amt++;
+// }
+
+/*-----------------------------------------------------*/
+
+Console.WriteLine("Give your username:");
+
+string user = Console.ReadLine().ToLower();
+
+Console.WriteLine($"{user} give your password:");
+
+string password = Console.ReadLine().ToLower();
+
+if (user == "noname" && password == "nopass")
+{
+    Console.WriteLine("Welcome!");
+}
+else
+{
+    Console.WriteLine("Wrong username or password!");
+
+}
+
+Console.ReadLine();
